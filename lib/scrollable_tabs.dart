5f4fef74f8c70f3f4f3515b46c9f6d4f5c883e52
chacha <867571123@qq.com>
2019-04-06 09:12:00 +0800

    Update scrollable_tabs UI view

--- lib/scrollable_tabs.dart
+++ lib/scrollable_tabs.dart
@@ -7,9 +7,10 @@ enum TabsStyle {
 }
 
 class _Page {
-  const _Page({ this.icon, this.text});
+  const _Page({ this.icon, this.text, this.body });
   final IconData icon;
   final String text;
+  final Widget body;
 }
 
 const List<_Page> _allPages = <_Page>[
@@ -26,7 +27,7 @@ class ScrollableTabs extends StatefulWidget {
 
 class ScrollableTabsState extends State<ScrollableTabs> with SingleTickerProviderStateMixin {
   TabController _controller;
-  TabsStyle tabsStyle = TabsStyle.iconsAndText;
+  TabsStyle _tabsStyle = TabsStyle.iconsAndText;
   bool _customIndicator = false;
 
   @override
@@ -34,8 +35,100 @@ class ScrollableTabsState extends State<ScrollableTabs> with SingleTickerProvide
     super.initState();
   }
 
+  Decoration getIndicator() {
+    if (!_customIndicator) {
+      return UnderlineTabIndicator();
+    }
+
+    switch (_tabsStyle) {
+      case TabsStyle.iconsAndText:
+        return ShapeDecoration(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4.0)),
+            side: BorderSide(
+              color: Colors.white24,
+              width: 2.0,
+            ),
+          ) + const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4.0)),
+            side: BorderSide(
+              color: Colors.transparent,
+              width: 4.0,
+            ),
+          )
+        );
+      case TabsStyle.iconsOnly:
+        return ShapeDecoration(
+          shape: const CircleBorder(
+            side: BorderSide(
+              color: Colors.white24,
+              width: 4.0,
+            ),
+          ) + const CircleBorder(
+            side: BorderSide(
+              color: Colors.transparent,
+              width: 4.0,
+            ),
+          ),
+        );
+      case TabsStyle.textOnly:
+        return ShapeDecoration(
+          shape: const StadiumBorder(
+            side: BorderSide(
+              color: Colors.transparent,
+              width: 4.0,
+            ),
+          ) + const StadiumBorder(
+            side: BorderSide(
+              color: Colors.transparent,
+              width: 4.0,
+            ),
+          ),
+        );
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color iconColor = Theme.of(context).accentColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[],
+        bottom: TabBar(
+          controller: _controller,
+          isScrollable: true,
+          indicator: getIndicator(),
+          tabs: _allPages.map<Tab>((_Page page) {
+            assert(_tabsStyle != null);
+            switch (_tabsStyle) {
+              case TabsStyle.iconsAndText:
+                return Tab(text: page.text, icon: Icon(page.icon));
+              case TabsStyle.iconsOnly:
+                return Tab(icon: Icon(page.icon));
+              case TabsStyle.textOnly:
+                return Tab(text: page.text);
+            }
+            return null;
+          }).toList(),
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: _allPages.map<Widget>((_Page page) {
+          return SafeArea(
+            top: false,
+            bottom: false,
+            child: Container(
+              key: ObjectKey(page.icon),
+              padding: const EdgeInsets.all(12.0),
+              child: page.body,
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
