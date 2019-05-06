c185e36a58364d4254b7a4c1079d2c1555f74c6e
chacha <867571123@qq.com>
2019-05-06 20:15:35 +0800

    Add launch image, update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  var imageView: UIImageView?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -10,4 +12,22 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func applicationWillResignActive(_ application: UIApplication) {
+    // Sent when the application is about to move from active to inactive state. 
+    // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+    // or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, 
+    // and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    imageView = UIImageView.init(image: UIImage.init(named: "LaunchImage"))
+    self.window?.addSubview(imageView!)
+  }
+
+  override func applicationDidBecomeActive(_ application: UIApplication) {
+    if imageView != nil {
+      imageView?.removeFromSuperview()
+      imageView = nil
+    }
+  }
+
 }
